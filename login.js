ab3f8bff38a872b7af4a970d8f34c01e129ef53e
zaman4x101 <zaman4x101@gmail.com>
2026-03-12 13:23:33 +0600

    .

--- login.js
+++ login.js
@@ -1,6 +1,17 @@
-console.log("login functionality coming");
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("login-btn").addEventListener('click', function(){
-        console.log("Button clicked");
+console.log("DOM fully loaded");
+
+const loginBtn = document.getElementById("login-btn");
+if (loginBtn) {
+    loginBtn.addEventListener('click', function() {
+        let contactNumber = document.getElementById("input-number").value;
+        let passWord = document.getElementById('input-pass').value;
+
+        if (contactNumber == "01721316666" && passWord == "1234") {
+            alert("Succeed");
+            window.location.assign("/home.html");
+        } else {
+            alert("Failed");
+            return;
+        }
     });
-});
+}
